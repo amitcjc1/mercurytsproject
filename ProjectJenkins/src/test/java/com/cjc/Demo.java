fade8bbe7cbcbd8c9b9bd50bdd9cd5d662dc9c9d
amit <amit@DESKTOP-SIU2A5U>
2020-02-17 01:11:50 +0530

    Project Jenkins

---
+++ ProjectJenkins/src/test/java/com/cjc/Demo.java
@@ -0,0 +1,18 @@
+package com.cjc;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo 
+{
+	public static void main(String[] args) throws InterruptedException 
+	{
+		System.out.println("Hello");
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\amit\\Desktop\\chromedriver_win32 (4)\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.facebook.com/");
+	driver.manage().window().maximize();
+	
+}
+
+}
