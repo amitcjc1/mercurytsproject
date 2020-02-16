8c0393f16eb401a8423a6c578791f551cef72e10
amitcjc1 <61121166+amitcjc1@users.noreply.github.com>
2020-02-17 01:17:15 +0530

    updatedemo.java
    add in syso

--- ProjectJenkins/src/test/java/com/cjc/Demo.java
+++ ProjectJenkins/src/test/java/com/cjc/Demo.java
@@ -7,7 +7,7 @@ public class Demo
 {
 	public static void main(String[] args) throws InterruptedException 
 	{
-		System.out.println("Hello");
+		System.out.println("Hello This Is Amit");
 	System.setProperty("webdriver.chrome.driver", "C:\\Users\\amit\\Desktop\\chromedriver_win32 (4)\\chromedriver.exe");
 	WebDriver driver=new ChromeDriver();
 	driver.get("https://www.facebook.com/");
